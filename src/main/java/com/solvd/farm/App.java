5606fdfd5a95adc5b92e24d21bf20c1d027439e1
Viktor <vitya.malahov.95@gmail.com>
2023-12-21 13:28:21 +0300

    add maven

---
+++ src/main/java/com/solvd/farm/App.java
@@ -0,0 +1,13 @@
+package com.solvd.farm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
